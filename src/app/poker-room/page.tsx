02bd80a52ab23afebee44e97043eb9a7c39377a3
mr-sanjayarora <qym@innodata.com>
2025-10-17 13:19:09 +0530

    added players, table, buttons in bottom

--- src/app/poker-room/page.tsx
+++ src/app/poker-room/page.tsx
@@ -18,7 +18,9 @@ const page = () => {
     return (
         <div>
             <div className='topbar flex justify-between items-start p-5'>
-                <Image src={'/static/img/logo.png'} width={150} height={100} alt='Brand Logo' draggable={false} />
+                <div className=''>
+                    <Image src={'/static/img/logo.png'} className='absolute' width={150} height={100} alt='Brand Logo' draggable={false} />
+                </div>
                 <div className='flex-col items-center hidden md:flex'>
                     <ul className={`menu menu-horizontal bg-base-200 rounded-box ${sora.className} text-xs cut-border bg-gradient`}>
                         <li>
@@ -54,45 +56,128 @@ const page = () => {
                         </h2>
                     </div>
                 </div>
-                <ul className="menu bg-base-200 rounded-box navigation hidden md:block bg-gradient border border-t-2 border-b-2">
-                    <li>
-                        <a>
-                            <Image src={'/static/img/buttons/burger-bar.svg'} width={24} height={24} alt='Menu' />
-                        </a>
-                    </li>
-                    <span className='divider'></span>
-                    <li>
-                        <a>
-                            <Image src={'/static/img/buttons/stats.svg'} width={24} height={24} alt='Stats' />
-                        </a>
-                    </li>
-                    <span className='divider'></span>
-                    <li>
-                        <a>
-                            <Image src={'/static/img/buttons/poker.svg'} width={24} height={24} alt='Stats' />
-                        </a>
-                    </li>
-                    <span className='divider'></span>
-                    <li>
-                        <a>
-                            <Image src={'/static/img/buttons/chat.svg'} width={24} height={24} alt='Stats' />
-                        </a>
-                    </li>
-                </ul>
+                <div>
+                    <ul className="absolute right-5 menu bg-base-200 rounded-box navigation hidden md:block bg-gradient border border-t-2 border-b-2">
+                        <li>
+                            <a>
+                                <Image src={'/static/img/buttons/burger-bar.svg'} width={18} height={18} alt='Menu' />
+                            </a>
+                        </li>
+                        <span className='divider'></span>
+                        <li>
+                            <a>
+                                <Image src={'/static/img/buttons/stats.svg'} width={18} height={18} alt='Stats' />
+                            </a>
+                        </li>
+                        <span className='divider'></span>
+                        <li>
+                            <a>
+                                <Image src={'/static/img/buttons/poker.svg'} width={18} height={18} alt='Stats' />
+                            </a>
+                        </li>
+                        <span className='divider'></span>
+                        <li>
+                            <a>
+                                <Image src={'/static/img/buttons/chat.svg'} width={18} height={18} alt='Stats' />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div className='table'>
-                
+            <div className='game-table flex items-center justify-center xl:h-[calc(100vh-15vh)] h-[calc(100vh-30vh)] absolute top-0 left-0 w-full -translate-0'>
+                <div className='relative z-20 w-[calc(1000px-40px)] h-full'>
+                    <div className='absolute left-0 top-1/2 -translate-y-1/2'>
+                        <div className='text-center flex flex-col items-center'>
+                            <Image src={'/static/img/player3.png'} width={80} height={80} alt='player 1' />
+                            <div className='bg-player p-1 h-16 w-20 -mt-2 z-30 relative'>
+                                <span className={`${sora.className} text-xs`}>Ella</span>
+                                <h2 className={`${carterOne.className} text-sm`}>12000</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='absolute left-60 top-20'>
+                        <div className='text-center flex flex-col items-center'>
+                            <Image src={'/static/img/player2.png'} width={80} height={80} alt='player 1' />
+                            <div className='bg-player p-1 h-16 w-20 -mt-2 z-30 relative'>
+                                <span className={`${sora.className} text-xs`}>Zoe</span>
+                                <h2 className={`${carterOne.className} text-sm`}>12000</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='absolute right-60 top-20'>
+                        <div className='text-center flex flex-col items-center'>
+                            <Image src={'/static/img/player3.png'} width={80} height={80} alt='player 1' />
+                            <div className='bg-player p-1 h-16 w-20 -mt-2 z-30 relative'>
+                                <span className={`${sora.className} text-xs`}>Theo</span>
+                                <h2 className={`${carterOne.className} text-sm`}>12000</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='absolute right-0 top-1/2 -translate-y-1/2'>
+                        <div className='text-center flex flex-col items-center'>
+                            <Image src={'/static/img/player4.png'} width={80} height={80} alt='player 1' />
+                            <div className='bg-player p-1 h-16 w-20 -mt-2 z-30 relative'>
+                                <span className={`${sora.className} text-xs`}>You</span>
+                                <h2 className={`${carterOne.className} text-sm`}>12000</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='absolute left-60 bottom-15'>
+                        <div className='text-center flex flex-col items-center'>
+                            <Image src={'/static/img/player5.png'} width={80} height={80} alt='player 1' />
+                            <div className='bg-player p-1 h-16 w-20 -mt-2 z-30 relative'>
+                                <span className={`${sora.className} text-xs`}>Sophie</span>
+                                <h2 className={`${carterOne.className} text-sm`}>12000</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='absolute right-60 bottom-15'>
+                        <div className='text-center flex flex-col items-center'>
+                            <Image src={'/static/img/player1.png'} width={80} height={80} alt='player 1' />
+                            <div className='bg-player p-1 h-16 w-20 -mt-2 z-30 relative'>
+                                <span className={`${sora.className} text-xs`}>Harper</span>
+                                <h2 className={`${carterOne.className} text-sm`}>12000</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="radial-progress absolute -right-20 top-1/2 -translate-y-1/2" style={{"--value": 50, "--thickness": "0.3rem"}} aria-valuenow={50} role="progressbar">
+                        <div className='flex flex-col items-center'>
+                            <Image src={'/static/img/timer.svg'} width={15} height={5} alt=''/>
+                            <span className={`${carterOne.className} text-[10px] text-center mt-1 leading-3`}>16<br/>Sec</span>
+                        </div>
+                    </div>
+                </div>
+                <div className='absolute left-1/2 top-1/2 -translate-1/2 cards flex gap-2 z-10'>
+                    <div className='w-20 h-28 border rounded-md border-dashed text-xs flex justify-center items-center'>
+                        card 1
+                    </div>
+                    <div className='w-20 h-28 border rounded-md border-dashed text-xs flex justify-center items-center'>
+                        card 2
+                    </div>
+                    <div className='w-20 h-28 border rounded-md border-dashed text-xs flex justify-center items-center'>
+                        card 3
+                    </div>
+                    <div className='w-20 h-28 border rounded-md border-dashed text-xs flex justify-center items-center'>
+                        card 4
+                    </div>
+                    <div className='w-20 h-28 border rounded-md border-dashed text-xs flex justify-center items-center'>
+                        card 5
+                    </div>
+                </div>
+                <div className='absolute z-0'>
+                    <Image src={'/static/img/table-bed.svg'} width={1000} height={100} alt='poker-table' />
+                </div>
             </div>
-            <div className='footer-bar flex flex-row items-center md:gap-8 gap-4 absolute bottom-5 left-1/2 -translate-x-1/2'>
+            <div className='footer-bar flex flex-row items-center md:gap-8 gap-4 absolute bottom-2 left-1/2 -translate-x-1/2'>
                 <div className='md:flex gap-2 hidden'>
-                    <button className={`btn uppercase ${sora.className} w-28 h-14 text-sm skew-x-6 rounded-md bg-gradient border-l-0`}>Min</button>
-                    <button className={`btn uppercase ${sora.className} w-28 h-14 text-sm skew-x-6 rounded-md bg-gradient border-l-0`}>Half</button>
+                    <button className={`btn uppercase ${sora.className} w-28 h-14 text-sm btn-room-left`}>Min</button>
+                    <button className={`btn uppercase ${sora.className} w-28 h-14 text-sm btn-room-left`}>Half</button>
                 </div>
                 <div className='flex gap-2 items-baseline-last'>
-                    <button className={`btn uppercase ${sora.className} md:w-28 md:h-14 md:text-sm text-xs skew-x-6 rounded-md bg-gradient border-l-0`}>Check</button>
+                    <button className={`btn uppercase ${sora.className} md:w-28 md:h-14 md:text-sm text-xs btn-room-left`}>Check</button>
                     <div className='flex flex-col text-center'>
-                        <span className={`uppercase ${sora.className} mb-4 text-xs`}>Win: $3,74.0</span>
-                        <button className={`btn uppercase ${sora.className} md:w-40 md:h-14  md:text-sm text-xs skew-x-6 rounded-md bg-red`}>Fold</button>
+                        <span className={`uppercase ${sora.className} mb-4 text-xs btn-room py-2`}>Win: $3,74.0</span>
+                        <button className={`btn uppercase ${sora.className} md:w-40 md:h-14  md:text-sm text-xs btn-fold`}>Fold</button>
                     </div>
                 </div>
                 <div className='player-name md:w-36 w-26 flex flex-col justify-center items-center text-center '>
@@ -105,14 +190,14 @@ const page = () => {
                 </div>
                 <div className='flex gap-2 items-baseline-last'>
                     <div className='flex flex-col text-center'>
-                        <span className={`uppercase ${sora.className} mb-4 text-xs`}>Bet: $1,44.0</span>
-                        <button className={`btn uppercase ${sora.className} md:w-40 md:h-14 md:text-sm text-xs -skew-x-6 rounded-md bg-yellow`}>Rise</button>
+                        <span className={`uppercase ${sora.className} mb-4 text-xs btn-room-left py-2`}>Bet: $1,44.0</span>
+                        <button className={`btn uppercase ${sora.className} md:w-40 md:h-14 md:text-sm text-xs btn-rise`}>Rise</button>
                     </div>
-                    <button className={`btn uppercase ${sora.className} md:w-28 md:h-14 md:text-sm text-xs -skew-x-6 rounded-md bg-gradient border-r-0`}>Call</button>
+                    <button className={`btn uppercase ${sora.className} md:w-28 md:h-14 md:text-sm text-xs btn-room`}>Call</button>
                 </div>
                 <div className='md:flex gap-2 hidden items-baseline-last'>
-                    <button className={`btn uppercase ${sora.className} w-28 h-14 text-sm -skew-x-6 rounded-md bg-gradient border-r-0`}>Pot</button>
-                    <button className={`btn uppercase ${sora.className} w-28 h-14 text-sm -skew-x-6 rounded-md bg-gradient border-r-0`}>All In</button>
+                    <button className={`btn uppercase ${sora.className} w-28 h-14 text-sm btn-room`}>Pot</button>
+                    <button className={`btn uppercase ${sora.className} w-28 h-14 text-sm btn-room`}>All In</button>
                 </div>
             </div>
         </div>
